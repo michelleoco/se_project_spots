b854e0d50c79d978adde3c6f95e16b541f180b29
Michelle O'Connell <michelle.oco@gmail.com>
2024-10-16 21:52:03 -0600

    new update for my project

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Elephant portrait",
+    link: "http://127.0.0.1:5500/images/1-elephant-portrait-photo-by-michelle-oconnell.jpg",
+  },
+  {
+    name: "Elephants in the Okavango Delta",
+    link: "http://127.0.0.1:5500/images/2-elephants-okavango-photo-by-michelle-oconnell.jpg",
+  },
+  {
+    name: "Giraffes at a watering hole",
+    link: "http://127.0.0.1:5500/images/3-giraffes-waterhole-photo-by-michelle-oconnell.jpg",
+  },
+  {
+    name: "Oryx in Sossusvlei",
+    link: "http://127.0.0.1:5500/images/4-oryx-photo-by-michelle-oconnell.jpg",
+  },
+  {
+    name: "Zebras in the Etosha Pan",
+    link: "http://127.0.0.1:5500/images/5-zebra-photo-by-michelle-oconnell.jpg",
+  },
+  {
+    name: "Waterhole",
+    link: "http://127.0.0.1:5500/images/6-waterhole-photo-by-michelle-oconnell.jpg",
+  },
+];
