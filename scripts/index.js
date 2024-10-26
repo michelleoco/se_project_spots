b37155471e187d03c1161d36bc21307801d0210e
Michelle O'Connell <michelle.oco@gmail.com>
2024-10-26 08:48:03 -0600

    First submission

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,32 @@ const initialCards = [
   {
     name: "Elephant portrait",
     link: "http://127.0.0.1:5500/images/1-elephant-portrait-photo-by-michelle-oconnell.jpg",
+    alt: "Elephant portrait by Michelle OConnell",
   },
   {
     name: "Elephants in the Okavango Delta",
     link: "http://127.0.0.1:5500/images/2-elephants-okavango-photo-by-michelle-oconnell.jpg",
+    alt: "Elephants in the Okavango Delta by Michelle OConnell",
   },
   {
     name: "Giraffes at a watering hole",
     link: "http://127.0.0.1:5500/images/3-giraffes-waterhole-photo-by-michelle-oconnell.jpg",
+    alt: "Giraffes at a watering hole by Michelle OConnell",
   },
   {
     name: "Oryx in Sossusvlei",
     link: "http://127.0.0.1:5500/images/4-oryx-photo-by-michelle-oconnell.jpg",
+    alt: "Oryx in Sossusvlei by Michelle OConnell",
   },
   {
     name: "Zebras in the Etosha Pan",
     link: "http://127.0.0.1:5500/images/5-zebra-photo-by-michelle-oconnell.jpg",
+    alt: "Zebras in the Etosha Pan by Michelle OConnell",
   },
   {
     name: "Waterhole",
     link: "http://127.0.0.1:5500/images/6-waterhole-photo-by-michelle-oconnell.jpg",
+    alt: "Waterhole by Michelle OConnell",
   },
 ];
 
@@ -36,6 +42,26 @@ const editModalNameInput = editModal.querySelector("#profile-name-input");
 const editModalDescriptionInput = editModal.querySelector(
   "#profile-description-input"
 );
+
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__title");
+  const cardImageEl = cardElement.querySelector(".card__image");
+  // select the image element
+
+  cardNameEl.textContent = data.name;
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.alt;
+  // assign values to the image src and alt
+
+  return cardElement;
+}
 
 function openModal() {
   editModalNameInput.value = profileName.textContent;
@@ -57,3 +83,8 @@ function handleEditFormSubmit(evt) {
 profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
 editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.append(cardElement);
+}
